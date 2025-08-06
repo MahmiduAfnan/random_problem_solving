c7b488f02fe2f3c99030a925dc3648a62bbedcd7
MahmiduAfnan <mahmidulhasanafnan@gmail.com>
2025-08-06 20:27:10 +0600

    Update MasterChef_finals.cpp

--- codeChef/MasterChef_finals.cpp
+++ codeChef/MasterChef_finals.cpp
@@ -1,1 +1,16 @@
+//https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TOP10
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+	int t; cin >> t; 
+	while(t--){
+	    int x; cin >> x;
+	    if (x <= 10){
+	        cout << "YES" << endl;
+	    }else {
+	        cout << "NO" << endl;
+	    }
+	}
+
+}
