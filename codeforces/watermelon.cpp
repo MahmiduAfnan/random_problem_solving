cb4be2867bde9307dec5334f79a8b584c576969f
MahmiduAfnan <mahmidulhasanafnan@gmail.com>
2025-08-06 20:18:52 +0600

    Update watermelon.cpp

--- codeforces/watermelon.cpp
+++ codeforces/watermelon.cpp
@@ -1,1 +1,17 @@
+//https://codeforces.com/contest/4/problem/A
 
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    int w ; cin >> w;
+    if (w%2 == 0 && w!=2){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    } 
+    
+    
+    return 0;
+}
