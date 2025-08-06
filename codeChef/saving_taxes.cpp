66527bba81e4f438f94565375ca740bf24cb0091
MahmiduAfnan <mahmidulhasanafnan@gmail.com>
2025-08-06 20:21:06 +0600

    Update saving_taxes.cpp

--- codeChef/saving_taxes.cpp
+++ codeChef/saving_taxes.cpp
@@ -1,1 +1,20 @@
+//https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TAXSAVING
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    int t; cin >>t;
+    while(t--){
+        int x,y;
+        cin >> x >> y;
+        cout << x - y << endl;
+    }
+    
+        
+    
+    
+    return 0;
+}
 
