60f220d7d258113798d37bbf6f09569057b6eca7
MahmiduAfnan <mahmidulhasanafnan@gmail.com>
2025-08-08 14:23:09 +0600

    Update 404 Not Found.cpp

--- codeChef/404 Not Found.cpp
+++ codeChef/404 Not Found.cpp
@@ -1,1 +1,13 @@
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ERROR404
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x; cin >> x;
+	if (x==404){
+	    cout << "NOT FOUND" << endl;
+	} else {
+	    cout << "FOUND"<< endl;
+	}
+}
