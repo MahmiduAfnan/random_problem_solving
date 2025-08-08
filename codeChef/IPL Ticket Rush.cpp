af774efe0a007f9861758a161b5b52151d327240
MahmiduAfnan <mahmidulhasanafnan@gmail.com>
2025-08-08 14:26:38 +0600

    Update IPL Ticket Rush.cpp

--- codeChef/IPL Ticket Rush.cpp
+++ codeChef/IPL Ticket Rush.cpp
@@ -1,1 +1,21 @@
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/IPLTRSH
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t; cin >> t;
+	while (t--){
+	    int n , m ;
+	    cin >> n >> m ;
+	    if (m>=n){
+	        cout << 0 << endl;
+	        
+	    }else {
+	        cout << n - m << endl;
+	    }
+	        
+	    
+	}
+
+}
