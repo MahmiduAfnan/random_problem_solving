cd3c85e090dd32b401e45eae1d3e8e297d75295f
MahmiduAfnan <mahmidulhasanafnan@gmail.com>
2025-08-08 14:21:48 +0600

    Update Determine the Score.cpp

--- codeChef/Determine the Score.cpp
+++ codeChef/Determine the Score.cpp
@@ -1,1 +1,15 @@
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DETSCORE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t; cin >> t;
+	while (t--){
+	    int x,n ;
+	    cin >> x >> n;
+	  int result = (x/10) * n ;
+	  cout << result << endl;
+	}
+
+}
