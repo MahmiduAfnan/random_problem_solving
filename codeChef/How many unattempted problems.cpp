b788693d2f7037e61cec8ead998b38fe1554bba5
MahmiduAfnan <mahmidulhasanafnan@gmail.com>
2025-08-08 14:20:57 +0600

    Update How many unattempted problems.cpp

--- codeChef/How many unattempted problems.cpp
+++ codeChef/How many unattempted problems.cpp
@@ -1,1 +1,13 @@
 
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PRACLIST
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int X , Y ; cin >> X >> Y ;
+	int  C = X - Y ;
+	cout << C << endl;
+
+}
+
