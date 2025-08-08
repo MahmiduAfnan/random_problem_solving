8068616178108b53fd3f9d220e9ce8aff05ea9aa
MahmiduAfnan <mahmidulhasanafnan@gmail.com>
2025-08-08 14:25:02 +0600

    Update Donation Drive.cpp

--- codeChef/Donation Drive.cpp
+++ codeChef/Donation Drive.cpp
@@ -1,1 +1,14 @@
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DONDRIVE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t ; cin >> t ;
+	while (t--){
+	    int x, y; cin >> x >> y ;
+	    int result = x - y ; 
+	    cout << result << endl;
+	}
+
+}
