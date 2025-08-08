16e27077120c5e4cfd5a7ee12807cb704a1d5bfd
MahmiduAfnan <mahmidulhasanafnan@gmail.com>
2025-08-08 14:24:06 +0600

    Update Off By One.cpp

--- codeChef/Off By One.cpp
+++ codeChef/Off By One.cpp
@@ -1,1 +1,12 @@
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/OFFBY1
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x , y;
+	cin >> x >> y;
+	int result = x+y;
+	cout <<  10 * result + 1 << endl;
+
+}
