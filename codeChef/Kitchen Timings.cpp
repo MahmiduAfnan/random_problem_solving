825dfb53a22af8720850648b4d665ca90141df68
MahmiduAfnan <mahmidulhasanafnan@gmail.com>
2025-08-08 14:25:52 +0600

    Update Kitchen Timings.cpp

--- codeChef/Kitchen Timings.cpp
+++ codeChef/Kitchen Timings.cpp
@@ -1,1 +1,16 @@
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/KITCHENTIME
 
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+int main() {
+	int t ; cin >> t ;
+	while (t--){
+	    int x, y; cin >> x >> y ;
+	    int result = y - x; 
+	    cout << result << endl;
+	}
+
+}
