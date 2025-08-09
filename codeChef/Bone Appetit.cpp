beb5614a4b3e376f981d386e7a3bf14bb07fcc6a
MahmiduAfnan <mahmidulhasanafnan@gmail.com>
2025-08-09 12:53:55 +0600

    Update Bone Appetit.cpp

--- codeChef/Bone Appetit.cpp
+++ codeChef/Bone Appetit.cpp
@@ -1,1 +1,12 @@
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BNE_APT
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m, x, y;
+    cin >> n >> m >> x >> y;
+    int total = (n * x) + (m * y);
+    cout << total << endl;
+    return 0;
+}
