130e23cc36176da32997f6e4aaa5bda43ef4af52
MahmiduAfnan <mahmidulhasanafnan@gmail.com>
2025-08-09 12:52:57 +0600

    Update Reach on Time.cpp

--- codeChef/Reach on Time.cpp
+++ codeChef/Reach on Time.cpp
@@ -1,1 +1,18 @@
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TIMELY
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t ; cin >> t;
+	while (t--){
+	    int x ; cin >> x ;
+	    if (x>=30){
+	        cout << "YES" << endl;
+	        
+	    } else {
+	        cout << " NO" << endl;
+	    }
+	}
+
+}
